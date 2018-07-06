29e210cbcc136abc42f1bb15f5bf73fa1a22a647
n4djib <n4djib@gmail.com>
2018-07-07 00:01:33 +0100

    new commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+# from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 
 from flask_admin import Admin, BaseView, expose
 from flask_admin.contrib.sqla import ModelView
-from flask_breadcrumbs import Breadcrumbs
+# from flask_breadcrumbs import Breadcrumbs
 
 
 
@@ -15,17 +15,17 @@ from flask_breadcrumbs import Breadcrumbs
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+# migrate = Migrate(app, db)
 bootstrap = Bootstrap(app)
-Breadcrumbs(app=app)
+# Breadcrumbs(app=app)
 
 login = LoginManager(app)
 login.login_view = 'login'
 
 admin = Admin(app, template_mode='bootstrap3')
 
-from app.models import User, Student, Phone, School, Branch, StudentSession, Session,\
-	 Grade, Semester, Unit, Module, Percentage, Type
+from app.models import User, Student, Phone, School, Branch, StudentSession, \
+	Session, Grade, Semester, Unit, Module, Percentage, Type
 
 # admin.add_view(ModelView(Student, db.session))
 # admin.add_view(ModelView(Phone, db.session))
